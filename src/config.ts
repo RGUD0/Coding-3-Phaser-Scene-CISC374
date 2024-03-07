d7b17886fc9c433b129b6e34ce23e23034308988
RGUD0 <114261602+RGUD0@users.noreply.github.com>
2024-03-07 18:28:19 -0500

    Copied over from Coding-2
    Base start

---
+++ src/config.ts
@@ -0,0 +1,40 @@
+import Phaser from "phaser";
+import MainScene from "./scenes/mainScene";
+import PreloadScene from "./scenes/preloadScene";
+
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 600;
+
+export const CONFIG = {
+    title: "My Untitled Phaser 3 Game",
+    version: "0.0.1",
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#ffffff",
+    scale: {
+        parent: "phaser-game",
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
+    },
+    scene: [PreloadScene, MainScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true,
+            gravity: { y: 300 },
+        },
+    },
+    input: {
+        keyboard: true,
+        mouse: true,
+        touch: true,
+        gamepad: false,
+    },
+    render: {
+        pixelArt: false,
+        antialias: true,
+    },
+};
